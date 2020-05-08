6aec6d1ea770249add3fc2f6bd77333dbb9c1b07
Huw Dawson <karakkaraz@gmail.com>
2020-05-08 12:05:48 +0100

    Added an example handler.

---
+++ UdonStringEvent/EventHandler.cs
@@ -0,0 +1,32 @@
+// Example Handler
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+using VRC.Udon.Common.Interfaces;
+
+/// <Summary>The handler of events.</Summary>
+public class EventHandler : UdonSharpBehaviour
+{
+    // The character who sent the event.
+    public string characterName;
+
+    // The event that was sent.
+    public string newEvent;
+
+    public void Handle()
+    {
+        string[] e = newEvent.Split(',');
+        Debug.Log("Got an event named " + e[0] + " with payload " + newEvent + " - am owner? " + ownerOfGame);
+
+        // Event type switch. Contains both state and view events. (Too many synced strings currently will break eventually.)
+        // TODO: Replace with a true event bus when network event payloads are added.
+        switch (e[0])
+        {
+            default:
+                Debug.Log("Got an event named " + e[0] + " but didn't know what to do with it.");
+                break;
+        }
+    }
+}

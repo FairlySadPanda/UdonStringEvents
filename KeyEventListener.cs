312aa2e1b4cd10e6d589135669728e5f6d661931
Extrys <extrysgo@gmail.com>
2020-05-22 16:50:54 +0200

    Update KeyEventListener.cs

--- KeyEventListener.cs
+++ KeyEventListener.cs
@@ -4,19 +4,53 @@ using UnityEngine.UI;
 using VRC.SDKBase;
 using VRC.Udon;
 using VRC.Udon.Common.Interfaces;
+using System;
 
-public class KeyEventListener : UdonSharpBehaviour
+public class DesktopKeyboardToggle : UdonSharpBehaviour
 {
+    public PoolingKeyListenerEntity keyListener;
     public KeyboardManager keyboard;
-
-    private void Update()
+    
+    void Awake() 
     {
-        if (Input.GetKeyDown(KeyCode.K))
-        {
-            if (Networking.LocalPlayer?.IsUserInVR())  
-                return;
-                
-            keyboard.Toggle();
-        }
+        if(Networking.LocalPlayer.IsUserInVR()) 
+            keyListener.OnPressed += keyboard.Toggle;    
+    }
+}
+
+
+public class PoolingKeyListenerEntity : UdonSharpBehaviour
+{
+    public PoolingKeyListenerSystem keyListenerSystem;
+    public KeyCode key;
+    public event Action OnPressed;
+
+    void Awake() => keyListenerSystem.AddEntityToSystem(this);
+    
+    void Check()
+    {
+        if(Input.GetKeyDown(key))
+            OnPresed?.Invoke();
+    }
+}
+
+
+public class PoolingKeyListenerSystem : UdonSharpBehaviour
+{
+    public PoolingKeyListenerEntity[] listeners;
+    int entityCount;
+    
+    void Update()
+    {
+        for(int i = 0; i < entityCount; ++i)
+            listeners[i].Check();
+    }
+    
+    public void AddEntityToSystem(PoolingKeyListenerEntity entity)
+    {
+        int currentLength = listeners.Length;
+        Array.Resize(ref listeners, currentLength + 1);
+        listeners[currentLength - 1] = entity;
+        entityCount++;
     }
 }
